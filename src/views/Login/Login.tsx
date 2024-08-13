0738fe404871b025e1f09099d0e5519aaaca062d
FrankCairampoma <ingfralch@gmail.com>
2024-08-13 18:27:05 -0500

    diseñando login

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -11,31 +11,77 @@ import {
   TouchableOpacity,
   Platform,
   SafeAreaView,
+  TextInput,
 } from 'react-native';
 import { useEffect } from 'react';
-import useColors from '../../hooks/useColors';
-import LogoSection from './../../components/LogoVertical';
 
-import LoginForm from './LoginForm';
+const { width, height } = Dimensions.get('window');
 
 function Login(): JSX.Element {
   const { navigate } = useNavigation();
-  const { colors } = useColors();
 
   return (
     <ScrollView
-      contentContainerStyle={{ flexGrow: 1, backgroundColor: colors.bg10 }}
+      contentContainerStyle={{ flexGrow: 1, backgroundColor: '#0A3649' }}
     >
-      <SafeAreaView
-        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-      >
+      <SafeAreaView style={styles.container}>
+        <Image
+          style={{
+            width: width * 0.35,
+            height: width * 0.35,
+            resizeMode: 'contain',
+            marginTop: height * 0.2,
+            alignSelf: 'center',
+            tintColor: 'white',
+            marginBottom: 0,
+          }}
+          source={require('../../assets/images/logo-icon_white.png')}
+        />
+        <Image
+          source={require('../../assets/images/logo-text_white.png')}
+          style={{ width: width * 0.5, resizeMode: 'contain', marginTop: -10 }}
+        />
         <View style={{ width: '90%', maxWidth: 300 }}>
-          <LogoSection />
-          <LoginForm />
+          <TextInput
+            style={{
+              height: 50,
+              backgroundColor: '#05222F',
+              borderRadius: 5,
+              color: 'white',
+              paddingHorizontal: 10,
+            }}
+            placeholder='Escribe tu correo '
+            placeholderTextColor='grey'
+            keyboardType='email-address'
+            autoCapitalize='none'
+          />
+          <TouchableOpacity style={{ marginTop: 20 }}>
+            <Text
+              style={{
+                color: 'grey',
+                fontWeight: 'bold',
+                fontSize: 16,
+                textAlign: 'center',
+                backgroundColor: '#DEDEDE',
+                padding: 10,
+                borderRadius: 5,
+                width: '100%',
+              }}
+            >
+              Continuar
+            </Text>
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Login;
