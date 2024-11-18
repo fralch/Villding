0af12d7874e5e205f87ca11e3237849e94d86696
Fralch <ingfralch@gmail.com>
2024-11-18 18:02:32 -0500

    mostrando proyecto de api

--- src/views/Projects/HomeProject.tsx
+++ src/views/Projects/HomeProject.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback} from 'react';
+import React, { useCallback } from "react";
 import {
   View,
   Text,
@@ -8,17 +8,21 @@ import {
   TextInput,
   TouchableOpacity,
   Dimensions,
-} from 'react-native';
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { useFocusEffect } from '@react-navigation/native';
-import Feather from '@expo/vector-icons/Feather';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import ProjectList from '../../components/Project/ProjectList';
-import ProjectListSearch from '../../components/Project/ProjectListSearch';
-import { getProjects } from '../../hooks/localStorageProject';
-import { removeProject } from '../../hooks/localStorageCurrentProject';
-import { getSesion } from '../../hooks/localStorageUser';
-import { useNavigation, NavigationProp } from '@react-navigation/native';
+} from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { useFocusEffect } from "@react-navigation/native";
+import Feather from "@expo/vector-icons/Feather";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import ProjectList from "../../components/Project/ProjectList";
+import ProjectListSearch from "../../components/Project/ProjectListSearch";
+import { getProjects } from "../../hooks/localStorageProject";
+import {
+  removeProject,
+  storeProject,
+} from "../../hooks/localStorageCurrentProject";
+import { getSesion } from "../../hooks/localStorageUser";
+import { useNavigation, NavigationProp } from "@react-navigation/native";
+import axios from "axios";
 
 interface Project {
   id: string;
@@ -31,35 +35,24 @@ interface Project {
 
 export default function HomeProject() {
   const { navigate } = useNavigation<NavigationProp<any>>();
-  const [search, setSearch] = React.useState<string>('');
+  const [search, setSearch] = React.useState<string>("");
   const [viewSearch, setViewSearch] = React.useState<boolean>(false);
   const [projects, setProjects] = React.useState<Project[]>([]);
   const [filteredProjects, setFilteredProjects] = React.useState<Project[]>([]);
-  const [imageUserSesion , setImageUserSesion] = React.useState<string>();
+  const [imageUserSesion, setImageUserSesion] = React.useState<string>();
+  const [user, setUser] = React.useState<any>();
 
-  const screenWidth = Dimensions.get('window').width;
-  const headerWidth = Dimensions.get('window').width;
-
-  
-
-  useFocusEffect(
-    useCallback(() => {
-      getProjects().then((StoredProjects) => {
-        if (StoredProjects) {
-          setProjects(StoredProjects);
-          setFilteredProjects(StoredProjects);
-        }
-      });
-    }, [])
-  );
+  const screenWidth = Dimensions.get("window").width;
+  const headerWidth = Dimensions.get("window").width;
 
   React.useEffect(() => {
-    getSesion().then((StoredSesion : any) => {
+    getSesion().then((StoredSesion: any) => {
       let sesion = JSON.parse(StoredSesion);
-       console.log(sesion.uri);
-       setImageUserSesion(sesion.uri);
+      console.log(sesion.id);
+      setImageUserSesion(sesion.uri);
+      setUser(sesion);
     });
-  }, [ ])
+  }, []);
 
   useFocusEffect(
     useCallback(() => {
@@ -68,8 +61,9 @@ export default function HomeProject() {
         let sesion = JSON.parse(StoredSesion);
         console.log(sesion.uri);
         setImageUserSesion(sesion.uri);
+        setUser(sesion);
       });
-  
+
       return () => {
         // Limpieza si es necesario
       };
@@ -79,7 +73,7 @@ export default function HomeProject() {
   React.useEffect(() => {
     removeProject();
     const filtered = projects.filter((project) => {
-      if (search === '') {
+      if (search === "") {
         return true;
       }
       return (
@@ -91,31 +85,113 @@ export default function HomeProject() {
     setFilteredProjects(filtered);
   }, [search, projects]);
 
+  useFocusEffect(
+    useCallback(() => {
+      getProjects().then((StoredProjects) => {
+        if (StoredProjects && StoredProjects.length > 0) {
+          setProjects(StoredProjects);
+          setFilteredProjects(StoredProjects);
+        } else {
+          // Si no hay proyectos almacenados, consulta al servidor
+          getSesion().then((StoredSesion: any) => {
+            const sesion = JSON.parse(StoredSesion);
+            if (sesion && sesion.id) {
+              setUser(sesion); // Establece el usuario
+              fetchProjectsFromServer(sesion.id).then((fetchedProjects) => {
+                setProjects(fetchedProjects);
+                setFilteredProjects(fetchedProjects);
+                // Opcional: Guarda los proyectos en localStorage
+                saveProjectsToLocalStorage(fetchedProjects);
+              });
+            } else {
+              console.error("No se encontró el ID del usuario en la sesión.");
+            }
+          });
+        }
+      });
+    }, [])
+  );
+
+  async function fetchProjectsFromServer(userId: string): Promise<Project[]> {
+    try {
+      const response = await axios.post(
+        "https://www.centroesteticoedith.com/endpoint/user/check-attachment",
+        { user_id: userId }, // Envía el ID del usuario en el cuerpo de la solicitud
+        {
+          headers: {
+            Authorization: `Bearer YOUR_TOKEN`, // Incluye el token si es necesario
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      // Mapea los datos de la API al formato de la interfaz
+      console.log(response.data);
+      const mappedProjects: Project[] = response.data.map((project: any) => ({
+        id: String(project.id),
+        title: project.name,
+        subtitle: project.location, // Asumiendo que `location` es un subtítulo adecuado
+        company: project.company,
+        image:
+          project.uri === null
+            ? "https://serviciosenlinea.mined.gob.ni/nic10cweb/assets/img/user.jpg"
+            : "https://www.centroesteticoedith.com/endpoint/images/projects/" +
+              project.uri, // Asumiendo que la imagen es la propiedad `uri`
+        week: calculateWeekDifference(project.start_date, project.end_date),
+      }));
+
+      return mappedProjects;
+    } catch (error) {
+      console.error("Error al obtener proyectos del servidor:", error);
+      return [];
+    }
+  }
+
+  async function saveProjectsToLocalStorage(
+    projects: Project[]
+  ): Promise<void> {
+    try {
+      await storeProject(JSON.stringify(projects));
+    } catch (error) {
+      console.error("Error al guardar proyectos en localStorage:", error);
+    }
+  }
+
+  // Función auxiliar para calcular semanas entre dos fechas
+  function calculateWeekDifference(startDate: string, endDate: string): number {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const msPerWeek = 1000 * 60 * 60 * 24 * 7;
+    return Math.round((end.getTime() - start.getTime()) / msPerWeek);
+  }
   return (
     <View style={[styles.container]}>
-      <ExpoStatusBar style='dark' />
+      <ExpoStatusBar style="dark" />
 
       {!viewSearch ? (
         <View style={[styles.header, { width: headerWidth }]}>
           <Image
-            source={require('../../assets/images/logo-tex-simple_white.png')}
+            source={require("../../assets/images/logo-tex-simple_white.png")}
             style={styles.title}
           />
           <View style={styles.headerIcons}>
             <TouchableOpacity onPress={() => setViewSearch(true)}>
               <Feather
-                name='search'
+                name="search"
                 size={26}
-                color='white'
+                color="white"
                 style={styles.icon}
               />
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() =>  navigate('EditUser')}>
-            <Image
-              source={ imageUserSesion ? { uri: imageUserSesion } : require('../../assets/images/user.png')}
-              style={styles.avatar}
-            />
+            <TouchableOpacity onPress={() => navigate("EditUser")}>
+              <Image
+                source={
+                  imageUserSesion
+                    ? { uri: imageUserSesion }
+                    : require("../../assets/images/user.png")
+                }
+                style={styles.avatar}
+              />
             </TouchableOpacity>
           </View>
         </View>
@@ -132,33 +208,35 @@ export default function HomeProject() {
         >
           <View style={[styles.headerSearch]}>
             <Feather
-              name='search'
+              name="search"
               size={26}
-              color='white'
+              color="white"
               style={[styles.icon, { marginRight: 0 }]}
             />
             <TextInput
               style={styles.input}
-              placeholder='Buscar...'
+              placeholder="Buscar..."
               value={search}
               onChangeText={setSearch}
-              placeholderTextColor='white'
+              placeholderTextColor="white"
             />
             <TouchableOpacity onPress={() => setViewSearch(false)}>
-              <MaterialIcons
-                name='cancel'
-                size={26}
-                color='white'
-              />
+              <MaterialIcons name="cancel" size={26} color="white" />
             </TouchableOpacity>
           </View>
         </View>
       ) : null}
 
       {!viewSearch ? (
-        <ProjectList projects={filteredProjects} />
-      ) : (
+        filteredProjects?.length > 0 ? (
+          <ProjectList projects={filteredProjects} />
+        ) : (
+          <Text>No hay proyectos disponibles</Text>
+        )
+      ) : filteredProjects?.length > 0 ? (
         <ProjectListSearch projects={filteredProjects} />
+      ) : (
+        <Text>No hay proyectos encontrados</Text>
       )}
     </View>
   );
@@ -167,29 +245,29 @@ export default function HomeProject() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#05222F', // Background color of the app
+    backgroundColor: "#05222F", // Background color of the app
   },
 
   header: {
-    backgroundColor: '#05222F',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    backgroundColor: "#05222F",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingHorizontal: 16,
     paddingTop: StatusBar.currentHeight || 20,
     height: 80,
     borderBottomWidth: 1,
-    borderBottomColor: '#0D465E',
+    borderBottomColor: "#0D465E",
     marginTop: 20,
   },
   title: {
     width: 120,
     height: 40,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
   headerIcons: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   icon: {
     width: 24,
@@ -204,25 +282,25 @@ const styles = StyleSheet.create({
   input: {
     flex: 1,
     height: 40,
-    backgroundColor: '#05222F',
+    backgroundColor: "#05222F",
     borderRadius: 10,
     paddingHorizontal: 10,
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
   headerSearch: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#05222F',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#05222F",
     paddingHorizontal: 10,
     borderWidth: 1,
-    borderColor: '#0D465E',
+    borderColor: "#0D465E",
     marginHorizontal: 0,
     marginBottom: 20,
   },
   debug: {
-    borderColor: 'red',
+    borderColor: "red",
     borderWidth: 1,
   },
 });
